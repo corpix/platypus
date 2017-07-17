579f0adf8a8a87ac468a0d0fe2f029471dfcceca
Dmitry Moskowski <me@corpix.ru>
2017-07-17 17:13:00 +0000

    Lower keys before they will be added to warehouse

--- warehouse/warehouse.go
+++ warehouse/warehouse.go
@@ -18,21 +18,27 @@ type Warehouse struct {
 
 func (w Warehouse) Set(key []string, value interface{}) {
 	w.data.Set(
-		strings.Join(key, keyDelimiter),
+		strings.ToLower(
+			strings.Join(key, keyDelimiter),
+		),
 		value,
 	)
 }
 
 func (w Warehouse) Get(key []string) (interface{}, bool) {
 	return w.data.Get(
-		strings.Join(key, keyDelimiter),
+		strings.ToLower(
+			strings.Join(key, keyDelimiter),
+		),
 	)
 }
 
 func (w Warehouse) FindPrefix(key []string) map[string]interface{} {
 	var (
 		res       = map[string]interface{}{}
-		joinedKey = strings.Join(key, keyDelimiter)
+		joinedKey = strings.ToLower(
+			strings.Join(key, keyDelimiter),
+		)
 	)
 
 	w.data.IterCb(
