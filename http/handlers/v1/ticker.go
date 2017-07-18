4b7184c57000dc4a7657394f8f07f1ba3a10f81b
Dmitry Moskowski <me@corpix.ru>
2017-07-18 22:56:02 +0000

    Initial websockets support

--- http/handlers/v1/ticker.go
+++ http/handlers/v1/ticker.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/gobwas/ws"
+	"github.com/gobwas/ws/wsutil"
 	"github.com/gorilla/mux"
 
 	"github.com/cryptounicorns/market-fetcher-http/datasources"
@@ -16,13 +18,57 @@ func Mount(r *mux.Router, d *datasources.Datasources, l logger.Logger) {
 	v.HandleFunc(
 		"/tickers",
 		func(rw http.ResponseWriter, r *http.Request) {
-			res, err := json.Marshal(
-				d.Ticker.Store.FindPrefix([]string{}),
-			)
+			// FIXME: Very dirty, requires refactoring
+			conn, _, _, err := ws.UpgradeHTTP(r, rw, nil)
 			if err != nil {
 				l.Error(err)
+				return
 			}
-			rw.Write(res)
+
+			go func() {
+				var (
+					res []byte
+					err error
+				)
+				defer conn.Close()
+
+				res, err = json.Marshal(
+					d.Ticker.Store.FindPrefix([]string{}),
+				)
+				if err != nil {
+					l.Error(err)
+					return
+				}
+				err = wsutil.WriteServerMessage(
+					conn,
+					ws.OpBinary,
+					res,
+				)
+				if err != nil {
+					l.Error(err)
+					return
+				}
+
+				for v := range d.Ticker.Feed {
+					res, err = json.Marshal(v)
+					if err != nil {
+						l.Error(err)
+						return
+					}
+
+					// FIXME: Should we read from client to mitigate
+					// high memory consumption attacks?
+					err = wsutil.WriteServerMessage(
+						conn,
+						ws.OpBinary,
+						res,
+					)
+					if err != nil {
+						l.Error(err)
+						return
+					}
+				}
+			}()
 		},
 	)
 }
